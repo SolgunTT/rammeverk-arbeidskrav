9bd921b73dea329ed47025673b516add8823d9e5
SolgunTT <solgun.tveit@gokstadakademiet.nogit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.emailgit config --global user.namegit config --global user.name ditt>
2023-10-08 11:02:08 +0200

    added: max. 5 minus points per word and repeat API words if done before 2 min

--- jsrammeverk-arb.krav-1/src/components/GameComponent.jsx
+++ jsrammeverk-arb.krav-1/src/components/GameComponent.jsx
@@ -4,74 +4,70 @@ import CountdownComponent from "./CountdownComponent";
 import InputField from "./InputField";
 import PointComponent from "./PointComponent";
 
-
 const GameComponent = ({ playerName }) => {
   const [wordIndex, setWordIndex] = useState(0);
   const words = Høst.ord;
   const currentWord = words[wordIndex];
   const [inputValue, setInputValue] = useState("");
-  const [matchCount, setMatchCount] = useState(0);
   const [totalMatchCount, setTotalMatchCount] = useState(0);
-  const [consecutiveCorrect, setConsecutiveCorrect] = useState(0); 
-
-
-
-
+  const [consecutiveCorrect, setConsecutiveCorrect] = useState(0);
+  const [minusMatchCount, setMinusMatchCount] = useState(0);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === " ") {
         e.preventDefault();
-        if (wordIndex < words.length) {
-          if (inputValue === currentWord) {
-            setTotalMatchCount(totalMatchCount + 50); // Add 50 points for a correct word
-            if (consecutiveCorrect === 2) {
-              // If it's the third consecutive correct word, add 100 extra points
-              setTotalMatchCount(totalMatchCount + 100);
-              setConsecutiveCorrect(0); // Reset the counter
-            } else {
-              setConsecutiveCorrect(consecutiveCorrect + 1);
-            }
+        if (inputValue === currentWord) {
+          setTotalMatchCount(totalMatchCount + 50); // Add 50 points for a correct word
+          if (consecutiveCorrect === 2) {
+            // If it's the third consecutive correct word, add 100 extra points
+            setTotalMatchCount(totalMatchCount + 100);
+            setConsecutiveCorrect(0); // Reset the counter
           } else {
-            setConsecutiveCorrect(0); // Reset the counter if the word is incorrect
+            setConsecutiveCorrect(consecutiveCorrect + 1);
           }
-          setWordIndex(wordIndex + 1);
-          setInputValue(""); // Clear the input field
+        } else {
+          setConsecutiveCorrect(0); // Reset the counter if the word is incorrect
         }
+        // Advance to the next word or loop back to the first
+        setWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+        setInputValue(""); // Clear the input field
       }
     };
-
+  
     document.addEventListener("keydown", handleKeyDown);
-
+  
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [wordIndex, words, currentWord, inputValue, totalMatchCount, consecutiveCorrect]);
-
-
- 
+  }, [currentWord, inputValue, totalMatchCount, consecutiveCorrect]);
+  
+  
+  
 
   const handleInputChange = (e) => {
     const enteredText = e.target.value;
     setInputValue(enteredText);
-  
-    console.log(enteredText.length);
 
-// Comparing the last letter in both variables
-    if (enteredText[enteredText.length - 1] === currentWord[enteredText.length - 1]) {
-      setMatchCount(enteredText.length);
-      setTotalMatchCount(totalMatchCount + 1)
+    // Comparing the last letter in both variables
+    if (
+      enteredText[enteredText.length - 1] ===
+      currentWord[enteredText.length - 1]
+    ) {
+// If congruent, then add a point
+      setTotalMatchCount(totalMatchCount + 1);
     } else {
-      setMatchCount(0); // Reset matchCount if the entered text doesn't match
-      setTotalMatchCount(totalMatchCount - 1); // Deduct a point for each wrong letter
+      // If non-congruent, subtract a point
+      setMinusMatchCount((prevCount) => prevCount + 1);
+// If more than 5 points are subtracted, then...
+      if (minusMatchCount >= 5) {
+        // Don't add - 1 to setTotalMatchCount
+      } else {
+        setTotalMatchCount(totalMatchCount - 1); // Deduct a point for each wrong letter
+      }
     }
-
     
   };
-
-  
-  
-
 
   return (
     <div className="game-div">
